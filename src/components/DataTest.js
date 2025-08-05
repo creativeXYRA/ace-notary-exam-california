acb20c004908006a5d039be9c7271f9ee1e308e7
Davina Chen <davi@DavinadeMacBook-Air.local>
2025-08-05 13:39:10 -0700

    Fix DataTest component - remove hooks causing error

---
+++ src/components/DataTest.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function DataTest() {
+  return (
+    <div style={{ 
+      background: 'red', 
+      color: 'white', 
+      padding: '20px', 
+      fontSize: '24px',
+      textAlign: 'center',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      zIndex: 9999
+    }}>
+      🚨 DataTest 正在運行！
+    </div>
+  );
+}
+
+export default DataTest;
